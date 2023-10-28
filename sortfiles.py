4656047a03b41912e14d533e7d0b44dc6a22c94b
Praween Chhetri <62017020+Pra-wnn@users.noreply.github.com>
2023-10-28 08:42:43 +0545

    Update sortfiles.py
    removed unnecessary comments

--- sortfiles.py
+++ sortfiles.py
@@ -24,18 +24,18 @@ def organize(path):
                 shutil.move(i,path+"\\"+"Otherfiles"+"\\")
             
             elif ext.endswith('.mp4'):
-                # print(ext)
+             
                 if not os.path.exists("mp4s"):
                     os.mkdir("mp4s")
                 shutil.move(i,path+"\\"+"mp4s"+"\\")
             elif ext.endswith(('.mkv','.webm','.rar')):
-                # print(ext)
+            
                 direct = 'directory'
                 parent = 'D:\\'
                 path1 = os.path.join(parent,direct)
                 os.makedirs(path1,exist_ok=True)   
                 #  pls use this if u want abs path done and check if it exists or not
-                # print(i)
+               
                 shutil.move(i,path1)
                 # print(path1)
 
